471a33ba7fffb6b46d18c8531ebae303843d0e11
ian1017 <mdawidamengo@gmail.com>
2020-06-30 02:11:08 +0300

    add navbar

--- photos/models.py
+++ photos/models.py
@@ -19,6 +19,11 @@ class Category(models.Models):
     )
     category = models.CharField(max_lenght=20, choices=IMAGE_CATEGORIES)
 
+    @classmethod
+    def search_by_category(cls, category):
+        category = cls.objects.filter(category = category)
+        return category
+
     def __str__(self):
         return self.category
 
@@ -27,7 +32,14 @@ class Image(models.Model):
     name = models.CharField(max_length=30)
     category = models.ForeignKey(Category)
     description = models.TextField()
-    location = models.ForeignKey(Location)
+    location = models.ForeignKey(Location, on_delete = models.CASCADE, related_name='img_location')
+
+    @classmethod
+    def search_images(cls,category):
+        images = cls.objects.filter(category = category)
+        return images
+
+
 
     def __str__(self):
         return self.name
