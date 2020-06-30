7af172be95c9e3fcbe6301ad25a33b16a9412cdf
ian1017 <mdawidamengo@gmail.com>
2020-06-30 10:06:29 +0300

    errors

--- photos/urls.py
+++ photos/urls.py
@@ -4,10 +4,12 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    url("^$", views.home, name="home")
-    url('^search/', views.search, name='search'), 
-    url('^location/', views.location, name='location')
+    url("^$",views.home,name="home"),
+    url('^search/', views.search,name='search'),
+    url('^location/', views.location, name='location'),
+    url(r'^copy/(\d+)',views.copy_image, name='copy_image'),
 ]
 
 if settings.DEBUG:
-    urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
--- photos/views.py
+++ photos/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .models import Image, Category, Location
+from .models import Image,Category,Location
 from django_countries import countries
 
-#Create your views here.
+
+
+# Create your views here.
 def home(request):
     images = Image.objects.all()
     return render(request, 'home.html', {"images":images})
@@ -15,15 +17,35 @@ def search(request):
         searched_images = Image.search_image(category)
         message = f"{search_term}" 
 
+
         return render(request, 'search.html',{"message":message, "images":searched_images})
     else:
         message = "No results."
         return render(request, 'search.html',{"message":message})
 
+
 def location(request):
-        if 'country' in request.GET and request.GET['country']:
-         country = request.GET.get('country')
+
+    if 'country' in request.GET and request.GET['country']:
+        location_object = request.GET.get('country')
+
+        location = Location.find_location(location_object)
+
+        images = Image.filter_by_location(location)
+        country = dict(countries)[location_object]
+        no_message = ''
+        message = f'Location: {country}'  
+
+        if not images:
+            no_message = "No images for this location"
+ 
+        return render(request,'location.html',{"images":images, "message":message, "no_message":no_message,})
+    else:
+        message = "Select A Country"
+        return render(request,'location.html',{"message":message})
 
 
+def copy_image(request,image_id):
+    Image.copy_image_url(image_id)
 
-    return render(request,'location.html') 
+    return redirect(home)
